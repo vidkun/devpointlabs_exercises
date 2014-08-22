6e15c6ac715b92f5c24331103b59987580b827bf
Carlos Garcia <carlos@par9golf.com>
2014-08-22 09:30:53 -0600

    Step 3. Repeat the matrix

--- examples/conways_game_of_life/carlos_conways_game_of_life_instructions.rb
+++ examples/conways_game_of_life/carlos_conways_game_of_life_instructions.rb
@@ -29,6 +29,10 @@ class Matrix
     @rows = 10
     @columns = 10
     @matrix_data = [] # Initialize just to know we will use this variable
+  end
+
+  def fill_with_data
+    @matrix_data = []
     (1..@rows).each do |row_number|
       new_row = []
       (1..@columns).each do |column_number|
@@ -47,8 +51,44 @@ class Matrix
       puts ""
     end
   end
-
 end
 
+class Menu
+  def clear_screen
+    system("clear")
+    # print "\e[2J\e[f" # A more hardcore way to clear the screen
+  end
+
+  def display
+    puts "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+    puts "Creating a Random Matrix of 10 x 10"
+    puts "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+  end
+
+  def prompt
+    puts 'Would you like another one?'
+    print '(y/n) :'
+    answer = gets.chomp().to_s.downcase
+    return answer
+  end
+end 
+
+
 my_matrix = Matrix.new
-my_matrix.display
+the_menu = Menu.new
+loop do
+  the_menu.clear_screen
+  sleep(0.1)
+  the_menu.display
+
+  my_matrix.fill_with_data
+  my_matrix.display
+  loop do
+    retry_answer = the_menu.prompt
+    if retry_answer == 'y'
+      break
+    elsif retry_answer == 'n'
+      exit
+    end
+  end
+end

e88f37f0acab429bd6c76de80502b33ffcdf2154
Carlos Garcia <carlos@par9golf.com>
2014-08-26 09:17:45 -0600

    updated variable names

--- examples/conways_game_of_life/carlos_conways_game_of_life_instructions.rb
+++ examples/conways_game_of_life/carlos_conways_game_of_life_instructions.rb
@@ -38,8 +38,8 @@ class Matrix
 
     @rows = self.validate_size(rows, 30)
     @columns = self.validate_size(columns, 100)
-    @matrix_data = [] # Initialize just to know we will use this variable
-    @updated_matrix_data = [] # Initialize updated matrix to display new iterations iterations
+    @current_board = [] # Initialize just to know we will use this variable
+    @living_cell_count_data = [] # Initialize updated matrix to display new iterations iterations
   end
 
   def validate_size(size_param, max_value)
@@ -53,22 +53,21 @@ class Matrix
   end
 
   def is_cell_alive?(x,y)
-    if @matrix_data[x] && @matrix_data[x][y]
-      @matrix_data[x][y] == 'O' ? true : false 
+    if @current_board[x] && @current_board[x][y]
+      @current_board[x][y] == 'O' ? true : false 
     else
       false
     end
   end
 
   def count_surrounding_living_cells
-    @updated_matrix_data = [] # Initialize updated matrix to display new iterations iterations
+    @living_cell_count_data = [] # Initialize updated matrix to display new iterations iterations
     (0..@rows-1).each do |row_number|
       new_row = []  
       (0..@columns-1).each do |column_number|
         surrounding_living_cells = 0
-        # Iterating through each cell, so now we have to check the surrounding ones
+        # Iterating through each cell, so now we have to check the surrounding ones 
         for adjecent_position in POSITIONS_TO_TEST
-          # binding.pry
           # Get the row coordinate adjacent to the cell we are testing
           row_to_test = row_number + adjecent_position.first 
           # Get the column coordinate adjacent to the cell we are testing
@@ -86,16 +85,13 @@ class Matrix
           # This is the adjacent cell's coordinates row_to_test, column_to_test
           # Test if the adjacent cell is alive and if so, add to the count of
           # surrounding cells that are alive
-          if self.is_cell_alive?(row_to_test, column_to_test)
-            # binding.pry
-            surrounding_living_cells += 1 
-          end
+          surrounding_living_cells += 1 if self.is_cell_alive?(row_to_test, column_to_test)
         end
         # This is the count of the living cells stored in the same tested cell
         new_row << surrounding_living_cells 
         # print surrounding_living_cells
       end
-      @updated_matrix_data << new_row # Once the new row is completed added to the updated_matrix_data
+      @living_cell_count_data << new_row # Once the new row is completed added to the updated_matrix_data
       puts ""
     end
   end
@@ -106,21 +102,21 @@ class Matrix
   #     Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
   def apply_rules_of_life_to_updated_matrix_data
     # binding.pry
-    old_matrix = @matrix_data.clone
-    @updated_matrix_data.each_with_index do |row, row_number|
+    old_matrix = @current_board.clone
+    @living_cell_count_data.each_with_index do |row, row_number|
       row.each_with_index.each_with_index do |surrounding_living_cells_count, column_number|
-        if @matrix_data[row_number][column_number] == 'O' # A live cell
-          case @updated_matrix_data[row_number][column_number]
+        if @current_board[row_number][column_number] == 'O' # A live cell
+          case @living_cell_count_data[row_number][column_number]
             when 0..1
-              @matrix_data[row_number][column_number] = ' ' # Dies of under population
+              @current_board[row_number][column_number] = ' ' # Dies of under population
             when 2..3 
-              @matrix_data[row_number][column_number] = 'O' # Lives on to the next generation
+              @current_board[row_number][column_number] = 'O' # Lives on to the next generation
             else
-              @matrix_data[row_number][column_number] = ' ' # Dies of over population
+              @current_board[row_number][column_number] = ' ' # Dies of over population
           end
         else # A dead cell
           # if surrounded by 3 living cells becomes alive by reproduction
-          @matrix_data[row_number][column_number] = 'O' if @updated_matrix_data[row_number][column_number] == 3
+          @current_board[row_number][column_number] = 'O' if @living_cell_count_data[row_number][column_number] == 3
         end
       end
     end
@@ -146,18 +142,18 @@ class Matrix
 
   def fill_with_data
     # srand(333)
-    @matrix_data = []
+    @current_board = []
     (1..@rows).each do |row_number|
       new_row = []
       (1..@columns).each do |column_number|
         new_row << VALUES[rand(2)] # Random of 0 or 1
       end
-      @matrix_data << new_row
+      @current_board << new_row
     end
   end
 
   def display
-    @matrix_data.each do |row| 
+    @current_board.each do |row| 
       row.each do |column|
         print column
       end 
@@ -188,7 +184,7 @@ class Menu
 
   def get_iterations
     print 'Please enter the number of iterations: '
-    rows = gets.chomp.to_i
+    iterations = gets.chomp.to_i
   end
 
   def prompt
@@ -210,13 +206,13 @@ loop do
   my_matrix = Matrix.new(dimensions.first, dimensions.last)
   my_matrix.fill_with_data
   iterations = the_menu.get_iterations
-  (0..iterations-1).each do 
+  (0..iterations-1).each do |iteration| 
     the_menu.clear_screen
     sleep(0.1)
     the_menu.display
 
     my_matrix.display
-    # binding.pry
+    binding.pry if iteration > 50
     my_matrix.count_surrounding_living_cells
     my_matrix.apply_rules_of_life_to_updated_matrix_data
     sleep(0.1)
